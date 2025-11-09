15a79d3afb91c49d2640ea1fc4127fabe110b945
Burakcalisici <calisici22@itu.edu.tr>
2025-11-09 20:24:24 +0300

    refactor: simplify movies table structure

--- database/table_creation/table_movies.sql
+++ database/table_creation/table_movies.sql
@@ -2,11 +2,8 @@
 
 CREATE TABLE IF NOT EXISTS movies (
     movie_id VARCHAR(12) PRIMARY KEY NOT NULL,
+    info_id INT,
     title_type VARCHAR(50),
     primary_title VARCHAR(255),
-    original_title VARCHAR(255),
-    is_adult BOOLEAN,
-    start_year INT,
-    end_year INT,
-    runtime_minutes INT
+    original_title VARCHAR(255)
 );
