a6f7c67f5b7c179fabc4584562e2559be2ab610d
Ezgi <ezzgissen@gmail.com>
2025-11-09 14:13:04 +0300

    Update alt_titles table

--- database/table_creation/table_alt_titles.sql
+++ database/table_creation/table_alt_titles.sql
@@ -1,14 +1,15 @@
 -- Ezgi
 
-CREATE TABLE IF NOT EXISTS alt_titles (
-    altTitles_id INT PRIMARY KEY NOT NULL,
-    movie_id VARCHAR(12),
+CREATE TABLE alt_titles (
+    altTitles_id INT PRIMARY KEY,
+    movie_id VARCHAR(50),
     ordering INT,
     localized_title VARCHAR(255),
     region VARCHAR(50),
     language VARCHAR(50),
-    types VARCHAR(255),
-    attributes VARCHAR(255),
+    types VARCHAR(100),
+    attributes VARCHAR(100),
     is_original_title BOOLEAN,
+    
     FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
 );
