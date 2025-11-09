98a4ff325d56f02a1d3ff9f96b81c19a43c0cb6a
Ezgi <ezzgissen@gmail.com>
2025-11-09 14:16:42 +0300

    update crew table logic and take ownership

--- database/table_creation/table_crew.sql
+++ database/table_creation/table_crew.sql
@@ -1,10 +1,11 @@
--- Caner
+-- Ezgi
 
-CREATE TABLE IF NOT EXISTS crew (
-    crew_id INT PRIMARY KEY NOT NULL,
-    movie_id VARCHAR(12),
-    person_id VARCHAR(12),
-    role VARCHAR(255),
-    FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
-    FOREIGN KEY (person_id) REFERENCES people(person_id)
+CREATE TABLE crew (
+    crew_id INT PRIMARY KEY,
+    person_id INT,         
+    director_id INT,        
+    role VARCHAR(100),
+    
+    FOREIGN KEY (person_id) REFERENCES people(person_id),
+    FOREIGN KEY (director_id) REFERENCES directors(director_id)
 );
