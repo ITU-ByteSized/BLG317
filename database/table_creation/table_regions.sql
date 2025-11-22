de558a6d198a60b0507f0db19c2b2e77f76d3865
Ezgi <ezzgissen@gmail.com>
2025-11-22 15:21:56 +0300

    Add NOT NULL to region_name and set utf8mb4 collation in regions table

--- database/table_creation/table_regions.sql
+++ database/table_creation/table_regions.sql
@@ -2,5 +2,6 @@
 
 CREATE TABLE IF NOT EXISTS regions (
     region_code VARCHAR(10) PRIMARY KEY,
-    region_name VARCHAR(100)
-);
+    region_name VARCHAR(100) NOT NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
