afbf03fd2449284d7c25dbdaa4978429dc540f5a
Ezgi <ezzgissen@gmail.com>
2025-11-30 17:14:59 +0300

    Update table_alt_titles.sql

--- database/table_creation/table_alt_titles.sql
+++ database/table_creation/table_alt_titles.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS alt_titles (
-    altTitles_id INT PRIMARY KEY,
-    movie_id VARCHAR(50),
+    alt_title_id INT AUTO_INCREMENT PRIMARY KEY,
+    production_id VARCHAR(12) NOT NULL,
     ordering INT,
     localized_title VARCHAR(255) NOT NULL,
     region_code VARCHAR(10),
@@ -8,6 +8,8 @@ CREATE TABLE IF NOT EXISTS alt_titles (
     types VARCHAR(100),
     attributes VARCHAR(100),
     is_original_title BOOLEAN,
-    
-    FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
-);
+    FOREIGN KEY (production_id) REFERENCES productions(production_id) ON DELETE CASCADE,
+    FOREIGN KEY (region_code) REFERENCES regions(region_code) ON DELETE SET NULL,
+    FOREIGN KEY (language_code) REFERENCES languages(language_code) ON DELETE SET NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
