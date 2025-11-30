361da2744d396e4e94611681814c119f0819177c
Burakcalisici <calisici22@itu.edu.tr>
2025-11-30 20:16:05 +0300

    Update staging_basics.sql

---
+++ database/staging_tables/staging_basics.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS staging_basics;
+CREATE TABLE staging_basics (
+    tconst VARCHAR(20),
+    titleType VARCHAR(50),
+    primaryTitle TEXT,
+    originalTitle TEXT,
+    isAdult TINYINT,
+    startYear VARCHAR(10),
+    endYear VARCHAR(10),
+    runtimeMinutes VARCHAR(10),
+    genres TEXT
+);
+LOAD DATA LOCAL INFILE 'C:/Users/burak/OneDrive/Masaüstü/title.basics.tsv'
+INTO TABLE staging_basics
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
